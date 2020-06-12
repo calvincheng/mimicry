9fd360be05345ee5e9a7517dde032beaeabcb91e
Calvin Cheng <wc15799@my.bristol.ac.uk>
2020-06-12 20:40:02 +0800

    Put firebaseConfig back into script.js

--- public/script.js
+++ public/script.js
@@ -43,8 +43,19 @@
 //         }
 //       },
 //
-import { config } from './config.js';
-let firebaseConfig = config.firebase;
+
+// import { config } from './config.js';
+// let firebaseConfig = config.firebase;
+const firebaseConfig = {
+        apiKey: "AIzaSyCFiWO2Shkk2jEQLp0ako4j7lgQ_DoOjnI",
+        authDomain: "mimicry-app.firebaseapp.com",
+        databaseURL: "https://mimicry-app.firebaseio.com",
+        projectId: "mimicry-app",
+        storageBucket: "mimicry-app.appspot.com",
+        messagingSenderId: "261960891324",
+        appId: "1:261960891324:web:35f3ecf09c327b9bbc89b9"
+};
+
 firebase.initializeApp(firebaseConfig);
 
 // ----------------------------
