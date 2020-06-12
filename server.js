0c9cad389fedd2be323500b739cdd18875c3dfc2
Calvin Cheng <wc15799@my.bristol.ac.uk>
2020-06-12 17:51:54 +0800

    Added basic server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log('Connected to server'));
